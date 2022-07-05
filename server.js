d2bc84be80f24543e74463635048cdd5ec3ec3b9
Danger-Noodley <103783878+Danger-Noodley@users.noreply.github.com>
2022-07-05 14:24:41 -0600

    adding database connection

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const db = knex({
   client: 'pg',
   version: '7.2',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
